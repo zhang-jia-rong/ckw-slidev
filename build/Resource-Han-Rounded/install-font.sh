e3d2e17a2438436df32b327043d94640df9f7977
Zhang Jia Rong <zhang-jia-rong@outlook.com>
2023-10-28 15:39:07 +0800

    refactor: redefine variables with double quotes
    Signed-off-by: Zhang Jia Rong <zhang-jia-rong@outlook.com>

--- build/Resource-Han-Rounded/install-font.sh
+++ build/Resource-Han-Rounded/install-font.sh
@@ -2,12 +2,12 @@
 #
 # Download the font to your computer and extract files if necessary. Font files
 # may have a .zip, .otf, or .ttf file extension.
-directory=$(cd $(dirname "${BASH_SOURCE[0]:-$0}") && pwd)
-proj_root=${directory%/build*}
+directory="$(cd $(dirname "${BASH_SOURCE[0]:-$0}") && pwd)"
+proj_root="${directory%/build*}"
 proj_name="Resource-Han-Rounded"
 base_archive_name="RHR-CN-0.990.7z"
 url="https://github.com/CyanoHao/${proj_name}/releases"
 url="${url}/download/v0.990/${base_archive_name}"
 curl -fsSLO --compressed "${url}"
 7z x "${base_archive_name}" -aoa -o"${proj_root}/styles/${proj_name}"
-rm -f './${base_archive_name}'
+rm -f '${base_archive_name}'
