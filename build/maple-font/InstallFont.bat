0ff017730312db74754eb38fe765373245251131
Zhang Jia Rong <zhang-jia-rong@outlook.com>
2023-10-30 11:54:37 +0800

    refactor: change the base URL name and set env var
    Signed-off-by: Zhang Jia Rong <zhang-jia-rong@outlook.com>

--- build/maple-font/InstallFont.bat
+++ build/maple-font/InstallFont.bat
@@ -6,23 +6,25 @@
   setlocal
   set "_PROJ_ROOT=%_DIRECTORY:\build="&:%
 
-  set "_BASE_ARCHIVE_NAME=MapleMono-SC-NF.zip"
-  set "_URL=https://github.com/subframe7536/%_PROJ_NAME%"
-  set "_URL=%_URL%/releases/download/v6.4/%_BASE_ARCHIVE_NAME%"
+  set "_BASE_TRUETYPE_ARCHIVE_NAME=MapleMono-SC-NF.zip"
+  set "_URL=https://github.com/subframe7536/%_PROJ_NAME%/releases/download"
+  set "_URL=%_URL%/v6.4/%_BASE_TRUETYPE_ARCHIVE_NAME%"
   curl -fsSLO "%_URL%"
-  tar -xf "%_BASE_ARCHIVE_NAME%" -C "%_PROJ_ROOT%\styles\%_PROJ_NAME%"
+  tar -xf "%_BASE_TRUETYPE_ARCHIVE_NAME%" -C "%_PROJ_ROOT%\styles\%_PROJ_NAME%"
 
-  set "_BASE_ARCHIVE_NAME=MapleMono-woff2.zip"
-  set "_URL=https://github.com/subframe7536/%_PROJ_NAME%"
-  set "_URL=%_URL%/releases/download/v6.4/%_BASE_ARCHIVE_NAME%"
+  set "_BASE_WOFF2_ARCHIVE_NAME=MapleMono-woff2.zip"
+  set "_URL=https://github.com/subframe7536/%_PROJ_NAME%/releases/download"
+  set "_URL=%_URL%/v6.4/%_BASE_WOFF2_ARCHIVE_NAME%"
   curl -fsSLO "%_URL%"
-  tar -xf "%_BASE_ARCHIVE_NAME%" -C "%_PROJ_ROOT%\styles\%_PROJ_NAME%"
+  tar -xf "%_BASE_WOFF2_ARCHIVE_NAME%" -C "%_PROJ_ROOT%\styles\%_PROJ_NAME%"
 
 :cleanUp
   del /q "7zr.exe"
-  del /q "%_BASE_ARCHIVE_NAME%"
+  del /q "%_BASE_TRUETYPE_ARCHIVE_NAME%"
+  del /q "%_BASE_WOFF2_ARCHIVE_NAME%"
   set "_DIRECTORY="
   set "_PROJ_ROOT="
   set "_PROJ_NAME="
-  set "_BASE_ARCHIVE_NAME="
+  set "_BASE_TRUETYPE_ARCHIVE_NAME="
+  set "_BASE_WOFF2_ARCHIVE_NAME="
   set "_URL="
--- build/maple-font/install-font.sh
+++ build/maple-font/install-font.sh
@@ -8,16 +8,16 @@ proj_name="maple-font"
 
 # Download and extract the Maple Mono SC NF fonts package.
 base_archive_name="MapleMono-SC-NF.zip"
-url="https://github.com/subframe7536/${proj_name}/releases"
-url="${url}/download/v6.4/${base_archive_name}"
+url="https://github.com/subframe7536/${proj_name}/releases/download/v6.4"
+url="${url}/${base_archive_name}"
 curl -fsSLO --compressed "${url}"
 7z x "${base_archive_name}" -aoa -o"${proj_root}/styles/${proj_name}"
 rm -f "${base_archive_name}"
 
 # Download and extract the Maple Mono fonts package.
 base_archive_name="MapleMono-woff2.zip"
-url="https://github.com/subframe7536/${proj_name}/releases"
-url="${url}/download/v6.4/${base_archive_name}"
+url="https://github.com/subframe7536/${proj_name}/releases/download/v6.4"
+url="${url}/${base_archive_name}"
 curl -fsSLO --compressed "${url}"
 7z x "${base_archive_name}" -aoa -o"${proj_root}/styles/${proj_name}"
 rm -f "${base_archive_name}"
